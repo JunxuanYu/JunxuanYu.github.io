1558c0026ae3ceb5d423a42cf2f2ca056f47a518
JunxuanYu <2157926389@qq.com>
2026-04-03 18:42:28 +0800

    2026-4-3-3

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/JunxuanYu.github.io/',
+  base: './',
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
